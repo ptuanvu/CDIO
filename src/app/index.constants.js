db81d40cce2fffc18662958dd3a25398fa1029cc
ptuanvu <clan_hunter@quangbinh.org>
2016-06-27 03:11:48 +0700

    F

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('cdio')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
